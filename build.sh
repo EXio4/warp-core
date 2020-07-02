f565cb5b60689cc7002257c05a9f9760e032170f
exio4 <me@exio4.xyz>
2020-07-01 21:21:17 -0300

    enable pthreads on webassembly

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-em++ -std=c++17 src/*.cxx src/App/*.cxx -Iinclude/ -O2 -g -s ALLOW_MEMORY_GROWTH=1 -s WASM=1 -o main.js
+em++ -std=c++17 src/*.cxx src/App/*.cxx -Iinclude/ -O2 -g -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=12 -s TOTAL_MEMORY=1024MB -s WASM=1 -o main.js
 
