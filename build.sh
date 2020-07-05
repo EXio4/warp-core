479fabe0ca33dce86d29ef0437d26b368bb6cd34
exio4 <me@exio4.xyz>
2020-07-05 00:52:34 -0300

    lower optimization level

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 em++ --bind -std=c++17 \
     src/*.cxx src/App/*.cxx -Iinclude/ \
-    -O2 -g \
+    -O3 -g \
     -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=12 \
-    -s TOTAL_MEMORY=1024MB -s ALLOW_MEMORY_GROWTH=1 \
+    -s INITIAL_MEMORY=1024MB -s ALLOW_MEMORY_GROWTH=1 \
     -s WASM=1 \
     -Wcast-align -Wover-aligned -s WARN_UNALIGNED=1 \
     -o main.js
